643d503648f0187b552360b2de4b2905d25b0a42
alecarcher <68913911+alecarcher@users.noreply.github.com>
2024-06-07 07:20:46 +0000

    Copilot third commit

---
+++ comment.js
@@ -0,0 +1,27 @@
+// Create web server and listen on port 8080
+// Create a http server
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+// Include the module
+var comment = require('./comment');
+
+// Create server and listen on port 8080
+http.createServer(function(req, res) {
+  // Set the content type to html
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  // Read the file
+  fs.readFile(path.join(__dirname, 'comment.html'), function(err, data) {
+    if (err) {
+      res.write('Error: ' + err);
+    } else {
+      res.write(data);
+    }
+    res.end();
+  });
+}).listen(8080);
+
+// Call the function to write comments
+comment.writeComments();
+console.log('Server running at http://');
